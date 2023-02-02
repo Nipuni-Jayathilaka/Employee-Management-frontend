a36640605963b1846eae8a8987f9221af566d0fa
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-02-02 06:56:44 +0530

    Create employee-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
