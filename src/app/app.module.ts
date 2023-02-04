9a9cb95f0d3abd30b96c354327cdc6f578487a22
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-02-04 12:07:05 +0530

    Finalize the frontend of Employee system management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import {FormsModule} from "@angular/forms";
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    ViewEmployeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
